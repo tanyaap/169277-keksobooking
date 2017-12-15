b3d41d4fe26a37a4730ea5fcd3d616db9b09b801
tanyaap <aptat@inbox.ru>
2017-12-15 18:11:05 +0700

    Личный проект: максимум подвижности

--- js/map.js
+++ js/map.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var MAIN_PIN_WIDTH = 40;
+  var MAIN_PIN_HEIGHT = 44;
   var map = document.querySelector('.map');
   var mapPins = map.querySelector('.map__pins');
   var mapPinMain = map.querySelector('.map__pin--main');
@@ -62,4 +64,36 @@
   popupClose.addEventListener('keydown', function (evt) {
     window.data.isEnterEvent(evt, closePopup);
   });
+
+  var address = document.querySelector('#address');
+
+  function onHandlerDrag(evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+      mapPinMain.style.left = (mapPinMain.offsetLeft - shift.x) + 'px';
+      mapPinMain.style.top = (mapPinMain.offsetTop - shift.y) + 'px';
+      address.value = (startCoords.x - MAIN_PIN_WIDTH / 2) + ', ' + (startCoords.y - MAIN_PIN_HEIGHT);
+    };
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  }
+  mapPinMain.addEventListener('mousedown', onHandlerDrag);
 })();
